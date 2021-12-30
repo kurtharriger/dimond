151301b0018cd9a5f1db93e7230c4256bed3e81b
Kurt Harriger <kurtharriger@gmail.com>
2021-12-30 13:53:14 -0700

    add ring server to example project

--- example/project.clj
+++ example/project.clj
@@ -4,6 +4,10 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [kurtharriger/dimond "0.1.0-SNAPSHOT"]]
+                 [kurtharriger/dimond "0.1.0-SNAPSHOT"]
+                 [com.stuartsierra/component "1.0.0"]
+                 [http-kit "2.3.0"]
+                 [ring/ring-core "1.9.4"]
+                 [spootnik/signal "0.2.4"]]
   :main example.core
   :repl-options {:init-ns example.core})
--- example/src/example/core.clj
+++ example/src/example/core.clj
@@ -1,8 +1,36 @@
 (ns example.core
-  (:require [dimond.core :as di]))
+  (:require [dimond.core :as di]
+            [org.httpkit.server :as httpkit]
+            [ring.middleware.params]
+            [clojure.edn :as edn]
+            [com.stuartsierra.component :as component]
+            [signal.handler :refer [on-signal]]))
+
+(defn greeter [name]
+  (str "Hello, " name))
+
+
+(defn app [greeter req]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    (greeter (get-in req [:query-params "name"] "World"))})
+
+(defn with-middleware [app]
+  (-> app 
+      (ring.middleware.params/wrap-params)))
+
+(defn start-server [app port]
+  (httpkit/run-server (with-middleware app) {:port port}))
+
 
 (defn -main
-  "I don't do a whole lot."
-  [& [args]]
-  (di/foo "world")
-  (println "Hello, World!"))
+  "Example ring server"
+  [& args]
+  (let [port (or (edn/read-string (first args)) 3000)
+        app  (partial app greeter)
+        stop-server  (start-server app port)
+        shutdown (fn [sig] (println (str "Caught " (name sig) " signal. Stopping Server")) (stop-server))]
+    (println (str "Server running on port " port))
+    (on-signal :term shutdown)
+    (on-signal :int  shutdown)
+    ))
